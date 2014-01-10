672cb51d513ea9be6d75acbd8ddbe017ea52a267
victorPP <v@chinolab.com>
2014-01-10 16:16:44 -0600

    modelos basicos
    creacion de modelos de gestiones basicas

---
+++ congreso/apps/gestiones/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class persona(models.Model):
+		nombre = models.CharField(max_length=50)
+		apeidos = models.CharField(max_length=100)
+		telefono = models.CharField(max_length=50)
+		direccion = models.CharField(max_length=100)
+		gestion = models.TextField()
+		observaciones = models.TextField()
+		status = models.BooleanField(default=false)
+
+		def __unicode__(self):
+
+				nombreCompleto = "%s %s" %(self.nombre, sellf.apeidos)
+					return nombreCompleto
